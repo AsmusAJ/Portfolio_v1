c719aaf494519c81bdd447f396a46a1be50d8f35
AJ Asmus <asmusaj@gmail.com>
2026-09-02 18:16:44 -0400

    wip

--- server/Data/DatabaseSeeder.cs
+++ server/Data/DatabaseSeeder.cs
@@ -2,23 +2,35 @@ public class DatabaseSeeder
 {
     public void SeedData(PortfolioDbContext db)
     {
-        if (db.Tags.Any() || db.WorkExperiences.Any())
+        var possibleTaskNames = new[]
+        {
+            "C#",
+            "ASP.NET",
+            "MVC",
+            "SQL",
+            "JavaScript",
+            "TypeScript",
+            "React",
+            "HTML",
+            "CSS",
+            "Python",
+            "WinForms",
+            "EF Core",
+            "Flask",
+        };
+
+        var existingTagNames = db.Tags.Select(tag => tag.Name).ToHashSet();
+        db.Tags.AddRange(
+            possibleTaskNames
+                .Where(name => !existingTagNames.Contains(name))
+                .Select(name => new Tag { Name = name })
+        );
+        db.SaveChanges();
+
+        if (db.WorkExperiences.Any() || db.Projects.Any())
         {
             return;
         }
-
-        db.Tags.AddRange(
-            new Tag { Name = "C#" },
-            new Tag { Name = "ASP.NET" },
-            new Tag { Name = "MVC" },
-            new Tag { Name = "SQL" },
-            new Tag { Name = "JavaScript" },
-            new Tag { Name = "TypeScript" },
-            new Tag { Name = "React" },
-            new Tag { Name = "HTML" },
-            new Tag { Name = "CSS" }
-        );
-        db.SaveChanges();
 
         var tags = db.Tags.ToList();
         db.WorkExperiences.AddRange(
@@ -26,21 +38,21 @@ public class DatabaseSeeder
             {
                 Title = "Software Engineer Intern",
                 Description =
-                    "Worked as a member of the dev team including the front-to-back creation of a new module using mvc.  Helped redesign the setup tutorials.  Also contributed to the development of a new feature.",
+                    "Developed an end-to-end React and ASP.NET Core feature that analyzes historical asset data and recommends alarm thresholds, helping users configure monitoring rules through an intuitive GUI.",
                 Company = "KCF Technologies",
                 CompanyUrl = "https://kcftech.com/",
                 Priority = 200,
-                Tags = tags.Where(t => new[] { "JavaScript", "HTML" }.Contains(t.Name)).ToList(),
+                Tags = tags.Where(t => new[] { "C#", "ASP.NET", "Python", "TypeScript", "React" }.Contains(t.Name)).ToList(),
             },
             new WorkExperience
             {
                 Title = "Programmer Analyst Intern",
-                Description = "Worked on various programming tasks and analysis projects.",
+                Description = "Designed database management system using Microsoft SQL Server, C#.NET and WinForms, facilitating standard naming conventions to enable cross-location data analysis.",
                 Company = "Howmet Aerospace",
                 CompanyUrl = "https://howmet.com/",
                 Priority = 100,
                 Tags = tags.Where(t =>
-                        new[] { "C#", "ASP.NET", "MVC", "SQL", "HTML" }.Contains(t.Name)
+                        new[] { "C#", "WinForms", "EF Core"}.Contains(t.Name)
                     )
                     .ToList(),
             }
@@ -51,19 +63,19 @@ public class DatabaseSeeder
             {
                 Title = "Portfolio Website",
                 Description =
-                    "Worked as a member of the dev team including the front-to-back creation of a new module using mvc.  Helped redesign the setup tutorials.  Also contributed to the development of a new feature.",
+                    "Developed a full-stack portfolio application using React, TypeScript, ASP.NET Core, and Entity Framework Core to deliver a responsive, data-driven web experience.",
                 Url = "https://kcftech.com/",
                 Priority = 200,
-                Tags = tags.Where(t => new[] { "JavaScript", "HTML" }.Contains(t.Name)).ToList(),
+                Tags = tags.Where(t => new[] { "C#", "ASP.NET", "TypeScript", "React" }.Contains(t.Name)).ToList(),
             },
             new Project
             {
-                Title = "Programmer Analyst Intern",
-                Description = "Worked on various programming tasks and analysis projects.",
+                Title = "TTRPG Campaign Manager App (Collaborative)",
+                Description = "Built a full-stack web application for managing collaborative tabletop RPG campaigns, including user authentication, campaign/character creation, session tracking, and secure access controls for multi-user data.",
                 Url = "https://howmet.com/",
                 Priority = 100,
                 Tags = tags.Where(t =>
-                        new[] { "C#", "ASP.NET", "MVC", "SQL", "HTML" }.Contains(t.Name)
+                        new[] { "Python", "Flask", "JavaScript" }.Contains(t.Name)
                     )
                     .ToList(),
             }
